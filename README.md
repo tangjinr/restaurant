56bae47b8c8e63547e69b00cf957aa4f14cf3810
tangjinr <tangpersonal@163.com>
2016-03-26 17:16:09 +0800

    提交说明

---
+++ README.md
@@ -0,0 +1,18 @@
+Web端：
+1、新建仓库Repository
+
+客户端：
+1、初始化
+git init
+
+2、将改动添加到暂存区
+git add . 或者git add ./xx.jsp
+
+3、提交
+git commit -m "提交说明"
+
+4、添加远程地址
+git remote add origin https://github.com/yourNmae/yourRepo.git
+
+4、将本地更改推送到远程master分支
+git push -u origin master
---
+++ my.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+#include <cctype>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <list>
+#include <deque>
+#define PI acos(-1.0)
+#define INF 0x7fffffff
+#define eps 1e-8
+#define NPOS string::npos
+#define ll long long
+#define ull unsigned long long
+#define maxn 10000 * 10
+#define mod 10007
+#define dif 0.000005
+using namespace std;
+
+int main()
+{
+<<<<<<< 7a70a211dd25a465592e3d5e8bda7b0dc5cd56a3
+=======
+<<<<<<< 7a70a211dd25a465592e3d5e8bda7b0dc5cd56a3
+=======
+	int a, b;
+>>>>>>> 增加了一些输入
+>>>>>>> 添加了输入
+	return 0;
+}
